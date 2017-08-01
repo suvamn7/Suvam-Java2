c577f1d17cd300c9b31a1beb776a12dd01df4f27
suvam <s.07.nayak@gmail.com>
2017-07-31 20:03:59 -0700

    hi

--- Calc/test/CalcTest.java
+++ Calc/test/CalcTest.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 import org.junit.Test;
 
 import Calc.Calc;
 import static org.junit.Assert.*;
-
-
 
 public class CalcTest {
 
@@ -47,13 +46,28 @@ public class CalcTest {
         assertEquals(3, c.depth());
     }
 
-    @Test(expected=Exception.class)
+    @Test
+    public void PopDepthTest() {
+        Calc c = new Calc();
+        c.push(3.0);
+        c.push(4.0);
+        c.push(3.0);
+        c.pop();
+        assertEquals(2, c.depth());
+        c.push(2.0);
+        assertEquals(3, c.depth());
+        c.pop();
+        c.pop();
+        assertEquals(1, c.depth());
+    }
+
+    @Test(expected = Exception.class)
     public void emptyPopTest() {
         Calc c = new Calc();
         c.pop();
     }
 
-    @Test(expected=Exception.class)
+    @Test(expected = Exception.class)
     public void tooManyPopTest() {
         Calc c = new Calc();
         c.push(3.0);
@@ -63,14 +77,15 @@ public class CalcTest {
         c.pop();
     }
 
-    @Test(expected=Exception.class)
+    @Test(expected = Exception.class)
     public void tooManyPushTest() {
         Calc c = new Calc();
-        for(int i = 0; i < 1e6; i++)
+        for (int i = 0; i < 1e6; i++) {
             c.push(100.0);
+        }
     }
 
-    @Test(expected=Exception.class)
+    @Test(expected = Exception.class)
     public void nopeekTest() {
         Calc c = new Calc();
         double v = c.peek();
@@ -91,17 +106,43 @@ public class CalcTest {
         assertEquals(5.0, c.peek(), EPSILON);
         c.multiply();
         assertEquals(20.0, c.peek(), EPSILON);
-        c.add();        
+        c.add();
         assertEquals(26.0, c.peek(), EPSILON);
     }
 
-    @Test(expected=Exception.class)
+    @Test(expected = Exception.class)
     public void underflowAddTest() {
         Calc c = new Calc();
         c.push(2.0);
         c.add();
     }
 
+    @Test(expected = Exception.class)
+    public void underflowSubtractTest() {
+        Calc c = new Calc();
+        c.push(2.0);
+        c.subtract();
+    }
+
+    @Test(expected = Exception.class)
+    public void underflowMultiplyTest() {
+        Calc c = new Calc();
+        c.push(2.0);
+        c.multiply();
+    }
+
+    @Test(expected = Exception.class)
+    public void underflowReciprocalTest() {
+        Calc c = new Calc();
+        c.reciprocal();
+    }
+
+    @Test(expected = Exception.class)
+    public void underflowDivideTest() {
+        Calc c = new Calc();
+        c.push(2.0);
+        c.divide();
+    }
 
     @Test
     public void singleSubtractTest() {
@@ -113,6 +154,33 @@ public class CalcTest {
     }
 
     @Test
+    public void singleAddTest() {
+        Calc c = new Calc();
+        c.push(2.0);
+        c.push(-10.0);
+        c.add();
+        assertEquals(-8.0, c.pop(), EPSILON);
+    }
+
+    @Test
+    public void singleMultiplyTest() {
+        Calc c = new Calc();
+        c.push(2.0);
+        c.push(-10.0);
+        c.multiply();
+        assertEquals(-20.0, c.pop(), EPSILON);
+    }
+
+    @Test
+    public void singleDivideTest() {
+        Calc c = new Calc();
+        c.push(2.0);
+        c.push(-10.0);
+        c.divide();
+        assertEquals(-.2, c.pop(), EPSILON);
+    }
+
+    @Test
     public void doubleMultiplyTest() {
         Calc c = new Calc();
         c.push(-1.0);
@@ -121,7 +189,8 @@ public class CalcTest {
         c.multiply();
         c.multiply();
         assertEquals(-370.0, c.pop(), EPSILON);
-                }
+    }
+
     @Test
     public void doubleDivideTest() {
         Calc c = new Calc();
@@ -130,7 +199,52 @@ public class CalcTest {
         c.push(2.0);
         c.divide();
         c.divide();
-        assertEquals(1.0, c.pop(),EPSILON);
+        assertEquals(1.0, c.pop(), EPSILON);
+
+    }
+
+    @Test
+    public void doubleAddTest() {
+        Calc c = new Calc();
+        c.push(25.0);
+        c.push(50.0);
+        c.push(2.0);
+        c.add();
+        c.add();
+        assertEquals(77.0, c.pop(), EPSILON);
+    }
+
+    @Test
+    public void doubleSubtractTest() {
+        Calc c = new Calc();
+        c.push(25.0);
+        c.push(30.0);
+        c.push(2.0);
+        c.subtract();
+        c.subtract();
+        assertEquals(-3.0, c.pop(), EPSILON);
+    }
+    @Test
+    public void compoundTest(){
+        Calc c = new Calc();
+        c.push(4.0);
+        c.push(3.0);
+        c.add();
+        c.push(7.0);
+        c.subtract();
+        c.push(5.0);
+        c.add();
+        c.push(5.0);
+        c.multiply();
+        c.push(3.0);
+        c.multiply();
+        c.push(25.0);
+        c.divide();
+        c.reciprocal();
+        c.push(75.0);
+        c.multiply();
+        assertEquals(25.0,c.pop(),EPSILON);
+        
                 
     }
 }
